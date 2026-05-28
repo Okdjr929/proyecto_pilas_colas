eca4bb10e407b9bd55c10af05f43f2550b995e19
Miqueas Ixcayá <otoxajil8@gmail.com>
2026-05-28 12:20:11 -0600

    Mejoré algunas inconsistencias en el login

--- Login y Menú Principal/Login y Menú Principal.cpp
+++ Login y Menú Principal/Login y Menú Principal.cpp
@@ -330,12 +330,12 @@ int main()
 {
 	SetConsoleOutputCP(CP_UTF8);
 	SetConsoleCP(CP_UTF8);
-	//login();
-	//PlaySound(TEXT("iron-man.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+	login();
+	PlaySound(TEXT("iron-man.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
 	system("cls");
-	//logo();
+	logo();
 	system("cls");
-	//pantallaInicio();
+	pantallaInicio();
 	system("cls");
 	color(7);
 	//MÚSICA AGREGADA
@@ -365,6 +365,7 @@ void gotoxy(int x, int y) {
 //Función para manejar el proceso de inicio de sesión. 
 // Permite al usuario ingresar su nombre y contraseña, y verifica los datos contra un archivo de texto. 
 // El usuario tiene un número limitado de intentos para ingresar correctamente.
+/*
 int login() {
 	system("title LOGIN");
 	char opcion;
@@ -457,8 +458,175 @@ int login() {
 	cout << "\n\n";
 
 	system("PAUSE");
-}
+}*/
 
+int login() {
+
+	system("title LOGIN");
+
+	char opcion;
+
+	int i = 0, j = 3;
+
+	string nombre;
+	string contra;
+	string linea;
+	string datos[2];
+
+	int contador = 0;
+
+	size_t posicion = 0;
+
+	do {
+
+		system("cls");
+
+		// ===== MARCO =====
+		for (int k = 20; k <= 136; k++) {
+
+			gotoxy(k, 3);
+			cout << "■";
+
+			gotoxy(k, 30);
+			cout << "■";
+		}
+
+		for (int k = 4; k <= 30; k++) {
+			gotoxy(20, k);
+			cout << "■";
+
+			gotoxy(136, k);
+			cout << "■";
+		}
+
+		// ===== TITULO =====
+		color(6);
+
+		imprimirCentradoModulo("LOGIN VENTA DE AIRE",6);
+
+		color(7);
+
+		imprimirCentradoModulo("===================================",8);
+
+		// ===== INTENTO =====
+		color(14);
+
+		imprimirCentradoModulo("Intento: " + to_string(j), 11);
+
+		// ===== NOMBRE =====
+		color(11);
+		imprimirCentradoModulo("Nombre:",15);
+		gotoxy(68, 16);
+		cin >> nombre;
+
+		// ===== CONTRASENIA =====
+		imprimirCentradoModulo("Contrasenia:",19);
+		gotoxy(68, 20);
+		cin >> contra;
+
+		color(7);
+
+		// ===== VALIDACION =====
+		fstream archivo("Documento.txt");
+
+		while (getline(archivo, linea)) {
+
+			while ((posicion = linea.find(",")) != string::npos) {
+
+				datos[contador] =
+					linea.substr(0, posicion);
+
+				linea.erase(0, posicion + 1);
+
+				contador++;
+			}
+
+			contador = 0;
+
+			if (datos[contador] == nombre) {
+				break;
+			}
+		}
+
+		// ===== LOGIN CORRECTO =====
+		if (datos[0] == nombre &&
+			datos[1] == contra)
+		{
+			color(10);
+			imprimirCentradoModulo("ACCESO CONCEDIDO",24);
+
+			color(7);
+
+			gotoxy(65, 27);
+
+			system("pause");
+
+			return EXIT_SUCCESS;
+		}
+
+		// ===== LOGIN INCORRECTO =====
+		else {
+			color(4);
+			imprimirCentradoModulo("DATOS INCORRECTOS",24);
+			i++;
+			j--;
+
+			// ===== SALIR =====
+			color(14);
+			imprimirCentradoModulo("Intentar otra vez [S/N] ",26);
+			cin >> opcion;
+			if (opcion == 'N' || opcion == 'n') {
+				color(4);
+				imprimirCentradoModulo("SALIENDO DEL LOGIN...",28);
+				color(7);
+				cout << "\n\n\n\n";
+				exit(EXIT_FAILURE);
+				return 0;
+			}
+		}
+
+		if (i >= 3) {
+			system("cls");
+			// ===== MARCO =====
+			for (int k = 20; k <= 136; k++) {
+
+				gotoxy(k, 3);
+				cout << "■";
+
+				gotoxy(k, 30);
+				cout << "■";
+			}
+
+			for (int k = 4; k <= 30; k++) {
+				gotoxy(20, k);
+				cout << "■";
+
+				gotoxy(136, k);
+				cout << "■";
+			}
+
+			// ===== ALERTA =====
+			color(4);
+
+			imprimirCentradoModulo(
+				"** ALERTA DE INTRUSO **",
+				18
+			);
+			gotoxy(0, 35);
+			color(7);
+			exit(EXIT_FAILURE);
+			return 0;
+		}
+
+	} while (opcion == 'S' || opcion == 's');
+
+
+	color(7);
+
+	gotoxy(65, 22);
+
+	system("pause");
+}
 //-----------SIMPLIFICACION DE OPCIONES DEL MENÚ Y SUBMENÚ--------------0
 //Función para mostrar un menú interactivo en la consola.
 int menu(string titulo, string opciones[], int n) {
@@ -667,7 +835,7 @@ void pantallaInicio() {
 		int colorRandom = (rand() % 15) + 1;
 		color(colorRandom);
 		gotoxy(xcol, 6); cout << titulo;
-		//Sleep(80)
+		Sleep(80);
 	}
 
 	color(7);
